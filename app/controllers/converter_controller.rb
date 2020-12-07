60b7e0a311cfdce651abd400108e5ac60deee0ad
AskBid <sergio.mrll@gmail.com>
2020-12-07 22:19:42 +0000

    added Open3 bech32 converter command

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,6 +1,6 @@
 class ConverterController < ApplicationController
   def bench
-  	byebug
-	render json: {empty: params[:hash]}
+  	output, status = Open3.capture2('bech32 stake', stdin_data: "e1#{params[:hash]}")
+	render json: {empty: output}
   end
 end
