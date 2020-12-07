cf80d2f8831569e8b510e0e1914df76812675271
AskBid <sergio.mrll@gmail.com>
2020-12-07 22:23:09 +0000

    working controller

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,6 +1,6 @@
 class ConverterController < ApplicationController
   def bench
   	output, status = Open3.capture2('bech32 stake', stdin_data: "e1#{params[:hash]}")
-	render json: {empty: output}
+	render json: {bech32: output.gsub("\n", '')}
   end
 end
