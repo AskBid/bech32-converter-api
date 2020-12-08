7979236e5d96a797ca640dc6abb919502b5ffb01
AskBid <sergio.mrll@gmail.com>
2020-12-08 12:06:19 +0000

    added condition for conversion from BECH32 rather than to

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # BECH32 Converter
 
-API to support other application that needs to query for an cardano hash address and convert it in BECH32 format using [bech32 binary](https://github.com/input-output-hk/bech32)
+Rails API to support other application that needs to query for an cardano hash address and convert it in BECH32 format using [bech32 binary](https://github.com/input-output-hk/bech32)
--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,6 +1,11 @@
 class ConverterController < ApplicationController
   def bench
-  	output, status = Open3.capture2('bech32 stake', stdin_data: "e1#{params[:hash]}")
+  	if params[:hash].include?('stake')
+  		output, status = Open3.capture2('bech32', stdin_data: "#{params[:hash]}")
+  		output = output.slice(2,output.length - 1)
+	else
+		output, status = Open3.capture2('bech32 stake', stdin_data: "e1#{params[:hash]}")
+	end
 	render json: {bech32: output.gsub("\n", '')}
   end
 end
